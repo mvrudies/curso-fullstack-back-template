3c9099f89d2ec513ba8ffff84f423934d810b240
Marcos Vinicius Lima <mrudies@MacBook-Air-de-Marcos.local>
2023-02-28 23:29:25 -0300

    INICIANDO O BD

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 class App {
 public app: express.Application;
@@ -7,6 +8,21 @@ public app: express.Application;
 public constructor() {
   this.app = express();
   this.app.use(cors());
+  this.intitMongoose();
+  this.connectDatabase();
+}
+
+private intitMongoose(): void {
+  mongoose.set('runValidators', true);
+}
+
+private connectDatabase(): void {
+  mongoose.connect('mongodb+srv://marcosrudies:hKUtZFeSkiptreMt@cluster0.ibjurqo.mongodb.net/curso_js?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
 }
 
 public listen(port: number): void {
